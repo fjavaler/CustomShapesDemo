d37129cc7db8e67ea6a844e94ba52b266c1e7c42
Frederick Javalera <frederickjavalera@gmail.com>
2021-09-29 10:50:19 -0600

    Initial Commit

---
+++ CustomShapesDemo/CustomShapesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapesDemoApp.swift
+//  CustomShapesDemo
+//
+//  Created by Frederick Javalera on 9/29/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
